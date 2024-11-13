f68f14ae9ebb668531aa8019268f5081171a16f2
ligthman <jamka888@gmail.com>
2024-11-13 12:05:20 +0800

    web server initialize

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { config } from "dotenv";
+config();
+
+const server = express();
+server.use(cors());
+server.use(bodyParser.json());
+const PORT = 8000;
+server.get("/", (_, res) => {
+  res.send("delivery backend server is working");
+});
+
+server.listen(PORT, () => {
+  console.log("working");
+});
